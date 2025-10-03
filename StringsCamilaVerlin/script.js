ff40334a47537e99648b2ee32302b2393ef46f29
camilasilvaverlin <132098121+camilasilvaverlin@users.noreply.github.com>
2025-10-03 16:18:55 -0300

    Add files via upload
    aula 9

--- StringsCamilaVerlin/script.js
+++ StringsCamilaVerlin/script.js
@@ -5,27 +5,33 @@ botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 function mostraPalavrasChave() {
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
-    const PalavrasChave = processaTexto(texto0);
-    
-    campoResultado.textContent = palavrasChave.join(", ")
+    const palavrasChave = processaTexto(texto);
+
+    campoResultado.textContent = palavrasChave.join(", ");
 }
 
 function processaTexto(texto) {
-    let palavras = texto.split(/\P{L}+/u);  
- 
- 
+    let palavras = texto.split(/\P{L}+/u);
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    console.log(ordenadas);
+    return ordenadas.slice(0, 10);
+}
+
+function contaFrequencias(palavras) {
     let frequencias = {};
     for (let i of palavras) {
-      frequencias[i] = 0;
-  
-      for (let j of palavras) {
-        if (i == j) {
-          frequencias[i]++;
+        frequencias[i] = 0;
+        for (let j of palavras) {
+            if (i == j) {
+                frequencias[i]++;
+            }
         }
-      }
     }
-  
-    console.log(frequencias);
-  
-    return palavras;
-  }
+    return frequencias;
+}
